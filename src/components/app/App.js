3f908ed2102776105139aa739054e413f8ad7bd6
smediums <smediums.me@gmail.com>
2022-01-11 15:37:25 +0300

    Start time stamp: 15:36, january 11, 2022

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,13 @@
-import './App.scss';
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import { Header, Home, Crew, Destination, Technology } from "../../components";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
