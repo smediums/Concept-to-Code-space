daaec19a79b2ee7bfbf9a6dd1f3fc82bfd606c27
smediums <smediums.me@gmail.com>
2022-01-11 17:26:47 +0300

    Mobile header and all navbar events complete. Moving to home page. Time stamp: 17:26; Jan 11, 2022

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import { Header, Home, Crew, Destination, Technology } from "../../components";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "./assets/logo.svg";
+import { ReactComponent as OpenToggle } from "./assets/icon-hamburger.svg";
+import { ReactComponent as CloseMenu } from "./assets/icon-close.svg";
+import { useState } from "react";
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [navShow, setNavShow] = useState(false);
 
-export default Header
+  const handleToggle = () => {
+    setNavShow(!navShow);
+  };
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <Logo />
+      </div>
+      <OpenToggle className="toggler" onClick={handleToggle} />
+      <nav className={navShow === true ? "showNav" : ""}>
+        <CloseMenu className="close" onClick={handleToggle} />
+        <ul className="navLinks">
+          <li>
+            <Link to="/" className="navLink">
+              <span>00</span> Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/crew" className="navLink">
+              <span>01</span> Crew
+            </Link>
+          </li>
+          <li>
+            <Link to="/destination" className="navLink">
+              <span>02</span> Destination
+            </Link>
+          </li>
+          <li>
+            <Link to="/technology" className="navLink">
+              <span>03</span> Technology
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
