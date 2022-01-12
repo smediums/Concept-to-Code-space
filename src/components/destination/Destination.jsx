82729d6c68adb276ae7b645b4b4a12a214cd00ac
smediums <smediums.me@gmail.com>
2022-01-12 09:10:32 +0300

    Finishing up destination page styling for mobile. Started at 08:45

--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -14,33 +14,35 @@ const Destination = ({ data }) => {
         <div className="theInfo">
           {/* Destination Img */}
           <img src={data[dataIndex].images.png} alt={data[dataIndex].name} />
-          {/* Destination choices */}
-          <ul className="places">
-            {data.map((place) => (
-              <li
-                key={place.name}
-                onClick={() => setDataIndex(data.indexOf(place))}
-              >
-                {place.name}
-              </li>
-            ))}
-          </ul>
-          {/* Summary of selected destination */}
           <div className="overview">
-            {/* Name */}
-            <h1 className="placeName">{data[dataIndex].name}</h1>
-            {/* Description */}
-            <p>{data[dataIndex].description}</p>
-            <div className="tripSummary">
-              {/* Distance */}
-              <div className="distance">
-                <p>Avg. distance</p>
-                <h4>{data[dataIndex].distance}</h4>
-              </div>
-              {/* Travel Time */}
-              <div className="travelTime">
-                <p>Est. Travel Time</p>
-                <h4>{data[dataIndex].travel}</h4>
+            {/* Destination choices */}
+            <ul className="places">
+              {data.map((place) => (
+                <li
+                  key={place.name}
+                  onClick={() => setDataIndex(data.indexOf(place))}
+                >
+                  {place.name}
+                </li>
+              ))}
+            </ul>
+            {/* Summary of selected destination */}
+            <div className="details">
+              {/* Name */}
+              <h1 className="placeName">{data[dataIndex].name}</h1>
+              {/* Description */}
+              <p>{data[dataIndex].description}</p>
+              <div className="tripSummary">
+                {/* Distance */}
+                <div className="distance">
+                  <p>Avg. distance</p>
+                  <h4>{data[dataIndex].distance}</h4>
+                </div>
+                {/* Travel Time */}
+                <div className="travelTime">
+                  <p>Est. Travel Time</p>
+                  <h4>{data[dataIndex].travel}</h4>
+                </div>
               </div>
             </div>
           </div>
